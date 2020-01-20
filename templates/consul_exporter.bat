7b5dc0984ec03abad55d78cf96eb04708c7cc6db
oper <suzhetao@gmail.com>
2020-01-20 14:13:50 +0800

    modified:   defaults/main.yml 	new file:   library/consul_acl_policy.py 	new file:   library/consul_acl_token.py 	modified:   tasks/Linux.yml 	new file:   tasks/Linux/policy.yml 	modified:   tasks/Linux/register.yml 	new file:   tasks/Linux/token.yml 	modified:   tasks/Win32NT/register.yml 	modified:   templates/client.json.j2 	modified:   templates/consul_exporter.bat 	modified:   templates/consul_exporter.j2 	modified:   templates/master.json.j2

--- templates/consul_exporter.bat
+++ templates/consul_exporter.bat
@@ -1,2 +1,2 @@
 set CONSUL_HTTP_TOKEN={{ consul_public_exporter_token }}
-c:\ProgramData\consul\bin\consul_exporter.exe --web.listen-address=":{{ consul_port_arg.exporter }}" --consul.server="{% if consul_arg.https %}https{% else %}http{% endif %}://localhost:{{ consul_port_arg.http }}" --no-consul.health-summary  --consul.allow_stale
+c:\ProgramData\consul\bin\consul_exporter.exe --web.listen-address=":{{ consul_port_arg.exporter }}" --consul.server="{% if consul_arg.https %}https{% else %}http{% endif %}://localhost:{{ consul_port_arg.http }}" --no-consul.health-summary  --consul.allow_stale --consul.insecure
